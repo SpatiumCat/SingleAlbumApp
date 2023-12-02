033e7eb65a3aac2ffe0248fd911dee17c585e699
Vladimir Baryshnikov <baryshnikovvladimirv@gmail.com>
2023-12-02 23:23:35 +0200

    made UI, viewModel and repository

---
+++ app/src/main/java/ru/netology/singlealbumapp/dto/Track.kt
@@ -0,0 +1,6 @@
+package ru.netology.singlealbumapp.dto
+
+data class Track(
+    val id: Long,
+    val file: String,
+)
---
+++ app/src/main/java/ru/netology/singlealbumapp/repository/AlbumRepository.kt
@@ -0,0 +1,7 @@
+package ru.netology.singlealbumapp.repository
+
+import ru.netology.singlealbumapp.dto.Album
+
+interface AlbumRepository {
+    suspend fun getAlbum(): Album
+}
